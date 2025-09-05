763bfad838f698cb2ccd8ab3d69e1b8d1b8e1c06
Edgar Borges <ed9arborges@gmail.com>
2025-09-05 11:47:56 +0100

    sessions token

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,5 +1,10 @@
+import prisma from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
 import { Request, Response } from "express"
 import { z } from "zod"
+import { authConfig } from "@/configs/auth"
+import { sign, SignOptions } from "jsonwebtoken"
 
 class SessionsController {
   async create(request: Request, response: Response) {
@@ -10,10 +15,35 @@ class SessionsController {
 
     const { email, password } = bodySchema.parse(request.body)
 
-    
-    return response.status(400).json({ email, password })
-    
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    })
 
+    if (!user) {
+      throw new AppError("Email or password incorrect", 401)
+    }
+
+    const isPasswordValid = await compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new AppError("Email or password incorrect", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({ role: user.role }, secret, {
+      subject: user.id,
+      expiresIn: expiresIn as SignOptions["expiresIn"],
+    })
+
+    return response
+      .status(200)
+      .json({
+        token,
+        user: { name: user.name, email: user.email, role: user.role },
+      })
   }
 }
 
